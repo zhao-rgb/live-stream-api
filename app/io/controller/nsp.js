d1075d8a2d1e89f7fa7c65e3c5b2669977424d4b
zhao-rgb <1019916061@qq.com>
2020-11-04 10:13:18 +0800

    socket通讯功能调试

--- app/io/controller/nsp.js
+++ app/io/controller/nsp.js
@@ -6,8 +6,16 @@ const Controller = require('egg').Controller;
 class NspController extends Controller {
   async test() {
     const { ctx, app } = this;
+    // 前端传过来的参数
+    const message = ctx.args[0];
+    console.log(message);
 
-    console.log(ctx);
+    // 当前的socket连接
+    const socket = ctx.socket;
+    // 取得socket的id
+    const id = socket.id;
+    // 向这个socket发送消息
+    socket.emit(id, '来自后端的消息');
   }
 }
 
